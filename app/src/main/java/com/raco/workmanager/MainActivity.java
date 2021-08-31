7a772aeb5dd09ef5873865b18ee14490a2e278e0
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-08-31 09:22:58 -0400

    Changes to MainActivity and SampleWorker classes.

--- app/src/main/java/com/raco/workmanager/MainActivity.java
+++ app/src/main/java/com/raco/workmanager/MainActivity.java
@@ -3,6 +3,13 @@ package com.raco.workmanager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Data data = new Data.Builder()
+                .putInt("number", 10)
+                .build();
+
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .setRequiresCharging(true)
+                .build();
+
+        OneTimeWorkRequest downloadRequest = new OneTimeWorkRequest.Builder(SampleWorker.class)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .setInitialDelay(5, TimeUnit.HOURS)
+                .addTag("download")
+                .build();
+
+        WorkManager.getInstance(this).enqueue(downloadRequest);
+
     }
 }
